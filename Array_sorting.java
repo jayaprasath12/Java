d68486692e03eb325d79ed7e4687cd0779bf9f3d
Jayaprasath <tjprasath1202@gmail.com>
2025-03-20 13:21:06 +0530

    deleted files

--- Array_sorting.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-public class Array_sorting {
-    public static void main(String[] args) {
-        String[] arr = {"Earth", "Air", "Water", "Fire"};
-
-        Arrays.sort(arr);
-        System.out.println("Changed to alphabetical order :" + Arrays.toString(arr));
-    }
-}
